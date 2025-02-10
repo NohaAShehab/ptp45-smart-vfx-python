dc0dcdbd711597bea58f26666bfeb3449e57c6a8
Noha Shehab <nohashehab.iti@gmail.com>
2025-02-10 14:47:29 +0200

    type of the variable

---
+++ sumnums.py
@@ -0,0 +1,12 @@
+
+year = 2025
+
+pii = 3.14
+
+name=  "noha"
+
+# i need to know how the data will be saved in the memory (DataType )
+
+print(type(name))
+print(type(pii))
+print(type(19))  # int - -> integer
